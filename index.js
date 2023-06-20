afda981d4b564bd318b8243704fca3f08adcf468
Mischel <susan.mwaura@student.moringaschool.com>
2023-06-20 15:19:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random > 0);
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4
+
+console.log(mod); 
+
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
+
+console.log(max); 
+
+
+
+
